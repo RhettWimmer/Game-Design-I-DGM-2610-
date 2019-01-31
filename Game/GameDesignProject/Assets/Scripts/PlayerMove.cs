59d2a7053d452a0d7d59421d24e2aff988b0cef4
Rhett <thedovahbrine@gmail.com>
2019-01-30 18:53:38 -0700

    Working

---
+++ Game/GameDesignProject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public int StartSpeed;
+    public int MoveSpeed;
+    public int JumpSpeed;
+
+    private void Update()
+    {
+        var startmove = Input.GetAxis("Horizontal") * Time.deltaTime * StartSpeed;
+        var move = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+        var jump = Input.GetAxis("Jump") * Time.deltaTime * JumpSpeed;
+        
+        transform.Translate(move, 0, 0);
+        transform.Translate(0, jump, 0);
+    }
+}
