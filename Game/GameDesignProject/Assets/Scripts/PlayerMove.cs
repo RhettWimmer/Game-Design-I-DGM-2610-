5b95abf80a1beb32372d8982d8fdd68600b0a3d8
Rhett <thedovahbrine@gmail.com>
2019-01-30 20:11:38 -0700

    Prototype down

--- Game/GameDesignProject/Assets/Scripts/PlayerMove.cs
+++ Game/GameDesignProject/Assets/Scripts/PlayerMove.cs
@@ -7,18 +7,32 @@ public class PlayerMove : MonoBehaviour
     public int StartSpeed;
     public int MoveSpeed;
     public int JumpSpeed;
+    public bool InAir;
 
     private void Update()
     {
         var startmove = Time.deltaTime * StartSpeed;
         var jump = Input.GetAxis("Jump") * Time.deltaTime * JumpSpeed;
-        
+        var move = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+
         transform.Translate(startmove, 0, 0);
         transform.Translate(0, jump, 0);
+        transform.Translate(move, 0, 0);
+    }
 
-        if (Input.GetKeyDown(KeyCode.A))
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Plat"))
         {
-            transform.Translate(startmove, 0, 0);
+            InAir = false;
+            print("Grounded");
         }
     }
+
+    private void OnCollisionExit(Collision other)
+    {
+        InAir = true;
+        print("In Air");
+    }
 }
+   
