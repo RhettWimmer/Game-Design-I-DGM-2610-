02ef33833fc0ded6fe8569366b3f0b0baa047758
Rhett <thedovahbrine@gmail.com>
2019-02-03 18:00:52 -0700

    Hour 2

--- Game/GameDesignProject/Assets/Scripts/CharacterController.cs
+++ Game/GameDesignProject/Assets/Scripts/CharacterController.cs
@@ -2,7 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
 
+    public float Speed;
+    public float JumpSpeed;
+    public float Gravity;
 
+    public float MoveX, MoveY;
+
+    private Vector3 moveDirection;
+    
+    public CharacterController Controller;
+    
+    public void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        
+    }
 }
