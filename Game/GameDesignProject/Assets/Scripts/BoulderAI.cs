5a4ca2f1cb100517b95b51018e9385b55d3cd047
Rhett <thedovahbrine@gmail.com>
2019-02-06 18:40:09 -0700

    Hour 2

---
+++ Game/GameDesignProject/Assets/Scripts/BoulderAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderAI : MonoBehaviour
+{
+
+    public GameObject Boulder;
+    public float SpawnTime = 5;
+    
+    private void Update()
+    {
+        SpawnTime -= Time.deltaTime;
+
+        if (SpawnTime <= 0)
+        {
+            Instantiate(Boulder);
+
+            SpawnTime = 5;
+        }
+    }
+}
---
+++ Game/GameDesignProject/Assets/Scripts/BoulderDESTROY.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderDESTROY : MonoBehaviour
+{
+
+    public int Time = 10;
+    
+    private void Start()
+    {
+        StartCoroutine(DestroyBoulder());
+    }
+
+    IEnumerator DestroyBoulder()
+    {
+        yield return new WaitForSecondsRealtime(Time);
+        Destroy(gameObject);
+    }
+}
