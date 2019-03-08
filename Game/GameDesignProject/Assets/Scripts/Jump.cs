2e9f00b45c6cedb000e37490143e55b74f0f0b20
Rhett <thedovahbrine@gmail.com>
2019-03-08 16:49:21 -0700

    reee

--- Game/GameDesignProject/Assets/Scripts/Jump.cs
+++ Game/GameDesignProject/Assets/Scripts/Jump.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     public float JumpValue;
-    public float Gravity = -9.81f;
+    public float Gravity;
     private Vector3 POS;
 
     private CharacterController JumpCont;
@@ -17,16 +17,28 @@ public class Jump : MonoBehaviour
     
     private void Update()
     {
+        POS.y -= Gravity * Time.deltaTime;
+               
+        if (JumpCont.isGrounded && Input.GetButton("Jump"))
+        {
+            POS.y = JumpValue;
+        } else if (JumpCont.isGrounded)
+        {
+            POS.y = 0;
+        }
 
+        JumpCont.Move(POS);
+        
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            POS.y = JumpValue * Time.deltaTime;
+            POS.y += JumpValue * Time.deltaTime;
         }
         else
         {
             POS.y -= Gravity * Time.deltaTime;
         }
 
-        JumpCont.Move(POS);
+        JumpCont.Move(POS);*/
     }
 }
---
+++ Game/GameDesignProject/Assets/Scripts/RunTest/CharacterMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterMove : MonoBehaviour
+{
+
+/*    public UnityEvent OnGround, OffGround;
+    public MoveBase CharacterMover;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        CharacterMover.Move(controller);
+    }*/
+}
