f1caceef7322b106a8b2ce52193aea70167cf891
Rhett <thedovahbrine@gmail.com>
2019-03-08 23:45:05 -0700

    Sprint 3 done

---
+++ Game/GameDesignProject/Assets/Scripts/EnemyLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLoader : MonoBehaviour
+{
+    public GameObject Enemy;
+    public float SpawnTimer = 2f;
+
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", SpawnTimer, SpawnTimer);
+    }
+
+    private void Spawn()
+    {
+        Instantiate(Enemy);
+    }
+}
