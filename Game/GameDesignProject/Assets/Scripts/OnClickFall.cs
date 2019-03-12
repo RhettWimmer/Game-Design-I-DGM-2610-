42bf28686e440cc7b84f12eac44a1bdd6feccf2f
Rhett <thedovahbrine@gmail.com>
2019-03-12 15:00:59 -0600

    working

---
+++ Game/GameDesignProject/Assets/Scripts/OnClickFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickFall : MonoBehaviour
+{
+    public Rigidbody FallItem;
+
+    private void Start()
+    {
+        FallItem = GetComponent<Rigidbody>();
+        FallItem.useGravity = false;
+    }
+
+   /*
+    private void OnMouseDown()
+    {
+        print("Clicked");
+        FallItem.useGravity = true;
+    }*/
+
+
+/*    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FallItem.useGravity = true;
+        }
+    }*/
+}
