9bd2d97af314db79c82056d3eff4efbd768d2700
Rhett <thedovahbrine@gmail.com>
2019-04-09 15:58:12 -0600

    Added train behavior

---
+++ Game/GameDesignProject/Assets/Scripts/TrainBevahiour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainBevahiour : MonoBehaviour
+{
+
+    public int TrainSpeed;
+    public bool Driving;
+
+    private void Start()
+    {
+        Driving = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Driving = true;
+    }
+
+    private void Update()
+    {
+        var TrainMove = Time.deltaTime * TrainSpeed;
+       
+        if (Driving == true)
+            transform.Translate(0, 0, TrainMove);
+    }
+}
+
+
+
+
+
+
+
+
+
+/*private void OnTriggerEnter(Collider other)
+    {
+        print("Train move");
+        var TrainMove = Time.deltaTime * TrainSpeed;
+        
+        transform.Translate(0, 0, TrainMove);
+    }
+}*/
