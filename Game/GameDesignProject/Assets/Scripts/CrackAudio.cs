84433a2bfcc09f588e925c2f669f05869f466f4a
Rhett <thedovahbrine@gmail.com>
2019-05-01 18:52:43 -0600

    Done tiem to film

---
+++ Game/GameDesignProject/Assets/Scripts/CrackAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackAudio : MonoBehaviour
+{
+    public AudioSource Crack;
+
+    private void Start()
+    {       
+        Crack = GetComponent<AudioSource>();
+        Crack.Pause();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Crack.Pause();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Crack.Play();
+        }
+        
+    }
+}
---
+++ Game/GameDesignProject/Assets/Scripts/LassoAudio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LassoAudio : MonoBehaviour
+{
+    public AudioSource LassoNoise;
+/*    public AudioSource Crack;*/
+
+    private void Start()
+    {
+        LassoNoise = GetComponent<AudioSource>();
+        LassoNoise.Pause();
+        
+/*        Crack = GetComponent<AudioSource>();
+        Crack.Pause();*/
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            LassoNoise.Play();
+/*            Crack.Pause();*/
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            LassoNoise.Pause();
+/*            Crack.Play();*/
+        }
+        
+    }
+}
